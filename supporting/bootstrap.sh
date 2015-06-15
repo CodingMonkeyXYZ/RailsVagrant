c76deae2b8c3da6635bb6fa4ee8403670cdd0ade
Alex <alex@wellbalanceddesign.com>
2015-06-15 18:07:48 +0100

    rethink - screw rvm, it sucks

--- supporting/bootstrap.sh
+++ supporting/bootstrap.sh
@@ -1,19 +1,26 @@
-#!/usr/bin/env bash
+function install {
+    echo installing $1
+    shift
+    apt-get -y install "$@" >/dev/null 2>&1
+}
 
-sudo apt-get -y update
+echo updating package information
+apt-add-repository -y ppa:brightbox/ruby-ng >/dev/null 2>&1
+apt-get -y update >/dev/null 2>&1
 
-sudo apt-get install -y apache2 curl git libmysqlclient-dev nodejs
+
+
+install apache2 curl git libmysqlclient-dev nodejs
 
 # Install mysqlserver and force it to accept the passwords so it doesn't pop-up a gui.
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password password oranges465'
-sudo debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password oranges465'
-sudo apt-get -y install mysql-server
+debconf-set-selections <<< 'mysql-server mysql-server/root_password password oranges465'
+debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password oranges465'
+install mysql-server
 
-# Import the key for RVM.
-gpg --import /vagrant/supporting/mpapis.asc
+install Ruby ruby2.2 ruby2.2-dev
 
-# Install RVM.
-curl -sSL https://get.rvm.io | bash -s stable
+# install gems
+gem install rails 
 
 # Append the public ssh key for bit bucket into known hosts to allow us to connect.
 cat /vagrant/supporting/bitbucket_keys  >> ~/.ssh/ssh_known_hosts
@@ -21,11 +28,3 @@ cat /vagrant/supporting/bitbucket_keys  >> ~/.ssh/ssh_known_hosts
 cat /vagrant/supporting/ssh-for-bitbucket  >> ~/.ssh/ssh-for-bitbucket
 
 cat /vagrant/supporting/config  >> ~/.ssh/config
-
-# Note to self: some background reading material:
-# Tips and stuff: https://www.debian-administration.org/article/503/How_do_you_manage_your_SSH_host_keys
-# The RVM install issue: https://github.com/rvm/rvm/issues/3110
-
-# N.b. part of the reason for splitting up these scripts is that some things like paths need
-# to be reloaded and 'source' doesn't seem to do that in this case but splitting up into
-# different scripts, does.
